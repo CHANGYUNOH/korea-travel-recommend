0ffe3f7cf6840f896b43d5082c6a20b21d98df47
노찬규 <no940901@gmail.com>
2024-11-30 14:32:07 +0900

    [노찬규] 지역 상세 페이지 중간 업데이트

--- src/app/travel/regions/[id]/page.tsx
+++ src/app/travel/regions/[id]/page.tsx
@@ -1,50 +1,87 @@
 import Link from 'next/link';
+import '@/styles/pages/regions.scss';
 
-export default function RegionDetail() {
+const RegionDetailPage = ({ params }: { params: { id: string } }) => {
+    const { id } = params;
+
     return (
         <div className='region-detail'>
             <div className='title-area'>
                 <div className='tit'>속초해수욕장</div>
                 <div className='sub'>강원 속초시</div>
-                <div className='title'>피서들의 사랑을 한몸에 받는 속초해수욕장</div>
-                <div className='top-area'>
-                    <button className='btn-like'>
-                        <i className='icon'/>
-                        <span className='num'>75</span>
-                    </button>
-                    <div className='btn-view'>
-                        <i className='icon'/>
-                        <span className='num'>12.7K</span>
-                    </div>
-                    <ul className='right-area'>
-                        <li className='right-area-list'>
-                            <button className='btn-bookmark'>
-                                <i className='icon'/>
-                            </button>
-                        </li>
-                        <li className='right-area-list'>
-                            <button className='btn-print'>
-                                <i className='icon'/>
-                            </button>
-                        </li>
-                        <li className='right-area-list'>
-                            <button className='btn-map'>
-                                <i className='icon'/>
-                            </button>
-                        </li>
-                        <li className='right-area-list'>
-                            <button className='btn-share'>
-                                <i className='icon'/>
-                                <span className='num'>15</span>
-                            </button>
-                        </li>
-                    </ul>
+                <div className='title'>
+                    <em>피서객들의 사랑을 한몸에 받는 속초해수욕장</em>
                 </div>
+            </div>
+            <div className='top-area'>
+                <ul className='top-area-left'>
+                    <li>
+                        <button className='btn-like'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                                 stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"/>
+                            </svg>
+                            <span className='num'>75</span>
+                        </button>
+                    </li>
+                    <li>
+                        <div className='btn-view'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                                 stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"/>
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+                            </svg>
+                            <span className='num'>12.7K</span>
+                        </div>
+                    </li>
+                </ul>
+                <ul className='top-area-right'>
+                    <li>
+                        <button className='btn-bookmark'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                                 stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"/>
+                            </svg>
+                        </button>
+                    </li>
+                    <li>
+                        <button className='btn-print'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                                 stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0 1 10.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0 .229 2.523a1.125 1.125 0 0 1-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0 0 21 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 0 0-1.913-.247M6.34 18H5.25A2.25 2.25 0 0 1 3 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 0 1 1.913-.247m10.5 0a48.536 48.536 0 0 0-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5Zm-3 0h.008v.008H15V10.5Z"/>
+                            </svg>
+                        </button>
+                    </li>
+                    <li>
+                        <button className='btn-map'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                                 stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z"/>
+                            </svg>
+                        </button>
+                    </li>
+                    <li>
+                        <button className='btn-share'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                                 stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z"/>
+                            </svg>
+                            <span className='num'>15</span>
+                        </button>
+                    </li>
+                </ul>
             </div>
             <div className='cont-area'>
                 <ul className='tab'>
                     <li className='tab-list'>
-                        <Link href="/" className='tab-list-item'>
+                        <Link href="/" className='tab-list-item is-active'>
                             <span className='txt'>사진보기</span>
                         </Link>
                     </li>
@@ -66,31 +103,49 @@ export default function RegionDetail() {
                 </ul>
                 <div className='gallery'>
                     <div className='top-area'>
-                        <button className='btn-line'>
-                            <i className='icon' />
+                        <button className='btn-img'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                                 stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"/>
+                            </svg>
                             <span className='txt'>사진등록하기</span>
                         </button>
                     </div>
                     <div className='img-area'>
                         <Link href='/'>
-                            <img />
+                            <img/>
                         </Link>
                     </div>
                     <div className='detail-view'>
                         <div className='top-area'>
                             <div className='title'>상세정보</div>
                             <button className='btn-modify'>
-                                <i className='icon' />
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                     strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round"
+                                          d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"/>
+                                </svg>
                                 <span className='txt'>관광정보 수정요청</span>
                             </button>
                         </div>
                         <div className='cont-area'>
                             <div className='text'>
-                                1976년 처음 개장한 속초해수욕장은 국내 대표적인 해수욕장 가운데 하나다. 속초 시내와 가깝고 고속버스터미널이 도보 거리에 있어 교통이 편리한 것이 장점이다. 속초해수욕장은 바닷물이 맑고 깨끗한 데다 수심이 얕고 경사가 완만해 매년 여름철이면 전국에서 모여든 피서객들로 북적 해진다. 특히 성수기인 7~8월에는 너른 해변이 발 디딜 틈도 없이 사람들로 가득 찬다. 해수욕장 주변은 음식점과 편의 시설이 잘 갖춰져 있다. 해변 뒤편에는 소나무 숲이 우거져 있으며 작은 공원과 산책로도 조성되어 있다. 휴가철이 지난 후에도 해수욕장에는 관광객들의 발길이 끊이지 않는다. 모래사장 곳곳에 설치된 조형물들이 계절에 관계없이 다채로운 볼거리를 선사하기 때문이다. 속초를 형상화한 ‘ㅅㅊ’ 이니셜과 세계 각국의 방향을 표시한 커다란 나침반, 생동감 있는 돌고래 조각 등이 눈길을 사로잡는다. 푸른 바다를 배경으로 한 대형 액자 프레임과 하늘을 향해 뻗은 천국의 계단은 이곳에서 가장 인기 있는 포토존이다. 매년 이곳에서는 관광객들에게 더욱 즐거운 추억을 선물하기 위해 버스킹, 여름 해변축제, 속초 수제맥주 축제를 즐길 수 있다. 여행시기에 맞추어 방문한다면 밴드 공연, 음악회 등의 공연행사를 즐길 수 있다.
+                                1976년 처음 개장한 속초해수욕장은 국내 대표적인 해수욕장 가운데 하나다. 속초 시내와 가깝고 고속버스터미널이 도보 거리에 있어 교통이 편리한 것이
+                                장점이다. 속초해수욕장은 바닷물이 맑고 깨끗한 데다 수심이 얕고 경사가 완만해 매년 여름철이면 전국에서 모여든 피서객들로 북적 해진다. 특히 성수기인
+                                7~8월에는 너른 해변이 발 디딜 틈도 없이 사람들로 가득 찬다. 해수욕장 주변은 음식점과 편의 시설이 잘 갖춰져 있다. 해변 뒤편에는 소나무 숲이 우거져
+                                있으며 작은 공원과 산책로도 조성되어 있다. 휴가철이 지난 후에도 해수욕장에는 관광객들의 발길이 끊이지 않는다. 모래사장 곳곳에 설치된 조형물들이 계절에
+                                관계없이 다채로운 볼거리를 선사하기 때문이다. 속초를 형상화한 ‘ㅅㅊ’ 이니셜과 세계 각국의 방향을 표시한 커다란 나침반, 생동감 있는 돌고래 조각 등이
+                                눈길을 사로잡는다. 푸른 바다를 배경으로 한 대형 액자 프레임과 하늘을 향해 뻗은 천국의 계단은 이곳에서 가장 인기 있는 포토존이다. 매년 이곳에서는
+                                관광객들에게 더욱 즐거운 추억을 선물하기 위해 버스킹, 여름 해변축제, 속초 수제맥주 축제를 즐길 수 있다. 여행시기에 맞추어 방문한다면 밴드 공연, 음악회
+                                등의 공연행사를 즐길 수 있다.
                             </div>
                             <button className='btn-more'>
                                 <span className='txt'>내용 더보기</span>
-                                <i className='icon' />
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                     strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15"/>
+                                </svg>
                             </button>
                         </div>
                         <div className='map-area'>
@@ -152,65 +207,6 @@ export default function RegionDetail() {
 
         </div>
     );
-}
-
-
-/*const RegionDetailPage = ({ params }: { params: { id: string } }) => {
-    const { id } = params;
-
-    return (
-        <div className=''>
-            <h1>{id} 지역 상세 페이지</h1>
-            <div className='title-area'>
-                <div className='tit'>속초해수욕장</div>
-                <div className='sub'>강원 속초시</div>
-                <div className='title'>피서들의 사랑을 한몸에 받는 속초해수욕장</div>
-                <div className='top-area'>
-                    <button className='btn-like'>
-                        <i className='icon'/>
-                        <span className='num'>75</span>
-                    </button>
-                    <div className='btn-view'>
-                        <i className='icon'/>
-                        <span className='num'>12.7K</span>
-                    </div>
-                    <ul className='right-area'>
-                        <li className='right-area-list'>
-                            <button className='btn-bookmark'>
-                                <i className='icon'/>
-                            </button>
-                        </li>
-                        <li className='right-area-list'>
-                            <button className='btn-print'>
-                                <i className='icon'/>
-                            </button>
-                        </li>
-                        <li className='right-area-list'>
-                            <button className='btn-map'>
-                                <i className='icon'/>
-                            </button>
-                        </li>
-                        <li className='right-area-list'>
-                            <button className='btn-share'>
-                                <i className='icon'/>
-                                <span className='num'>15</span>
-                            </button>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div className='cont-area'>
-                <ul className='tab'>
-                    <li className='tab-list'>
-                        <Link href="/" className='tab-list-item'>
-                            <span className='txt'>사진보기</span>
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-
-        </div>
-    );
 };
 
-export default RegionDetailPage;*/
+export default RegionDetailPage;
