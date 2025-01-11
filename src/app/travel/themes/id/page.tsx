31eedd03593a1d4fd08cf152c33357222adb8c50
노찬규 <no940901@gmail.com>
2025-01-11 18:51:39 +0900

    테마별 여행지 중간 업데이트

---
+++ src/app/travel/themes/id/page.tsx
@@ -0,0 +1,11 @@
+export default function ThemesDetailPage() {
+    return (
+        <>
+            <div className="themes">
+                <div className="themes-detail">
+                    
+                </div>
+            </div>
+        </>
+    )
+}
